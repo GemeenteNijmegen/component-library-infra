3b0a059e8f37e4d8bdb147113117b8d74b71f41b
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-05-16 13:20:18 +0200

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,12 @@
+const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/projen-project-type');
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  name: 'component-library-infra',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
