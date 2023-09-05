236138892cc9a06717ed64fe2ab351a8f5af7379
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-09-05 15:20:41 +0200

    feat: Generalize main ts
    No longer dependent on specific branches existing, grabs env from
    process.env, if none provided builds development, otherwise uses
    configuration file.

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,19 @@ import { Statics } from './Statics';
 
 export const app = new App();
 
-
-if ('BRANCH_NAME' in process.env == false || process.env.BRANCH_NAME == 'development') {
-  const config = getConfiguration('development');
-  new PipelineStack(app, 'component-library-development', {
-    projectName: Statics.projectName,
-    repository: `${Statics.repositoryOwner}/${Statics.repository}`,
-    env: config.deployFromEnvironment,
-    branchName: 'development',
-    configuration: config,
-  });
-} else if (process.env.BRANCH_NAME == 'sandbox') {
-  const config = getConfiguration('sandbox');
-  new PipelineStack(app, 'component-library-sandbox', {
-    projectName: Statics.projectName,
-    repository: `${Statics.repositoryOwner}/${Statics.repository}`,
-    env: config.deployFromEnvironment,
-    branchName: 'sandbox',
-    configuration: config,
-  });
+let branchName: string;
+if (!process.env.BRANCH_NAME || process.env.BRANCH_NAME == 'development') {
+  branchName = 'development';
+} else {
+  branchName = process.env.BRANCH_NAME;
 }
+const configuration = getConfiguration(branchName);
+new PipelineStack(app, `component-library-${branchName}`, {
+  projectName: Statics.projectName,
+  repository: `${Statics.repositoryOwner}/${Statics.repository}`,
+  env: configuration.deployFromEnvironment,
+  branchName,
+  configuration,
+});
 
 app.synth();
