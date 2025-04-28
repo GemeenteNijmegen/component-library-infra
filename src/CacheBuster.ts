9d543696dbc9c14f57b798d9e0fcbd978743c35c
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2025-04-28 15:58:39 +0200

    chore: add permission for invalidation

--- src/CacheBuster.ts
+++ src/CacheBuster.ts
@@ -13,17 +13,18 @@ interface CacheBusterProps {
 export class CacheBuster extends Construct {
   constructor(scope: Construct, id: string, props: CacheBusterProps) {
     super(scope, id);
-    const lambda = this.createLambda(props.distribution.distributionId);
+    const lambda = this.createLambda(props.distribution);
     this.setupTrigger(props.bucket, props.triggerKeys, lambda);
   }
 
-  createLambda(distributionId: string) {
+  createLambda(distribution: Distribution) {
     const lambda = new CacheInvalidationFunction(this, 'cacheInvalidator', {
       description: 'Invalidates a Cloudfront Cache. This is triggered by put events on a specific S3 key',
       environment: {
-        CLOUDFRONT_DISTRIBUTION_ID: distributionId,
+        CLOUDFRONT_DISTRIBUTION_ID: distribution.distributionId,
       },
     });
+    distribution.grantCreateInvalidation(lambda.grantPrincipal);
     return lambda;
   }
 
