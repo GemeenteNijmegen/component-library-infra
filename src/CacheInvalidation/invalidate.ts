6d6916a1c876cda8a5bc11742acd9d6bb1b32888
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2025-05-07 13:43:35 +0200

    chore: unique invalidation ids

--- src/CacheInvalidation/invalidate.ts
+++ src/CacheInvalidation/invalidate.ts
@@ -6,7 +6,7 @@ export function invalidateCommand(distributionId: string) {
   const command = new CreateInvalidationCommand({
     DistributionId: distributionId,
     InvalidationBatch: {
-      CallerReference: process.env.AWS_LAMBDA_FUNCTION_NAME,
+      CallerReference: `${Date.now()}`,
       Paths: {
         Quantity: items.length,
         Items: items,
