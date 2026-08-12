8f19870241fbcd93f77c74512c61905fe4858746
WietekeNijmegen <wietekenijmegen@gmail.com>
2026-08-12 18:24:10 +0200

    chore: upgrade deps  yarn to npm and js to ts

---
+++ .projenrc.ts
@@ -0,0 +1,26 @@
+import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  devDeps: [],
+  name: 'component-library-infra',
+  projenrcTs: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['development'],
+      labels: ['auto-merge'],
+    },
+  },
+  deps: [
+    '@gemeentenijmegen/projen-project-type',
+    '@gemeentenijmegen/cross-region-parameters',
+    '@gemeentenijmegen/aws-constructs',
+    '@gemeentenijmegen/utils',
+    '@gemeentenijmegen/dnssec-record',
+    '@types/aws-lambda',
+    '@aws-sdk/client-cloudfront',
+  ], /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
